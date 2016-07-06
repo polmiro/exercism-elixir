ecc25fb7f6350132d79da5e36c4a5ecdac94cf3d
Pol Miro <polmiro@gmail.com>
2016-07-05 22:13:38 -0700

    Sublist exercise

---
+++ sublist/sublist.exs
@@ -0,0 +1,28 @@
+require IEx;
+
+defmodule Sublist do
+  @doc """
+  Returns whether the first list is a sublist or a superlist of the second list
+  and if not whether it is equal or unequal to the second list.
+  """
+  def compare([], []), do: :equal
+  def compare([], _b),  do: :sublist
+  def compare(_a, []),  do: :superlist
+  def compare(a, b) do
+    cond do
+      a === b          -> :equal
+      sublist?(a, b) -> :sublist
+      sublist?(b, a) -> :superlist
+      true             -> :unequal
+    end
+  end
+
+  defp sublist?(a, b) when length(a) <= length(b) do
+    cond do
+      a === Enum.take(b, length(a)) -> true
+      true                          -> sublist?(a, tl(b))
+    end
+  end
+  defp sublist?(_, _), do: false
+
+end
