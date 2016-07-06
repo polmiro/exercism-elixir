799a8b0e44008796f7d019c96651f37b40829b2e
Pol Miro <polmiro@gmail.com>
2016-07-06 08:59:45 -0700

    Anagram exercise: sort base only once

--- anagram/anagram.exs
+++ anagram/anagram.exs
@@ -5,12 +5,13 @@ defmodule Anagram do
   @spec match(String.t, [String.t]) :: [String.t]
   def match(base, candidates) do
     base = normalize(base)
-    Enum.filter(candidates, &anagram?(&1, base))
+    sorted_base = Enum.sort(base)
+    Enum.filter(candidates, &anagram?(&1, base, sorted_base))
   end
 
-  defp anagram?(candidate, base) do
+  defp anagram?(candidate, base, sorted_base) do
     candidate = normalize(candidate)
-    base != candidate && Enum.sort(base) == Enum.sort(candidate)
+    base != candidate && sorted_base == Enum.sort(candidate)
   end
 
   defp normalize(str) do
