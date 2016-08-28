6130b5fe9660e64554fc7e847e00c8311b5f7f36
Pol Miro <polmiro@gmail.com>
2016-08-28 13:40:54 -0700

    ETL exercise

---
+++ etl/etl.exs
@@ -0,0 +1,24 @@
+defmodule ETL do
+  @doc """
+  Transform an index into an inverted index.
+
+  ## Examples
+
+  iex> ETL.transform(%{"a" => ["ABILITY", "AARDVARK"], "b" => ["BALLAST", "BEAUTY"]})
+  %{"ability" => "a", "aardvark" => "a", "ballast" => "b", "beauty" =>"b"}
+  """
+  @spec transform(map) :: map
+  def transform(input) do
+    input
+    |> Map.to_list
+    |> invert
+    |> List.flatten
+    |> Enum.into(%{})
+  end
+
+  defp invert(pairs) do
+    Enum.map(pairs, fn({key, values}) ->
+      Enum.map(values, fn(value) -> {String.downcase(value), key} end)
+    end)
+  end
+end
